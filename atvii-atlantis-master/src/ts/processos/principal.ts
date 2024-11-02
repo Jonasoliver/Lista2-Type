37022fab0a3a7c752c204cb1a312a68bf78d1c64
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-11-02 12:03:57 -0300

    adicionei a chamada do menu na principal

--- atvii-atlantis-master/src/ts/processos/principal.ts
+++ atvii-atlantis-master/src/ts/processos/principal.ts
@@ -3,6 +3,7 @@ import MenuPrincipal from "../menus/menuPricipal"
 import TipoCadastroCliente from "./tipoCadastroCliente"
 import TipoListagemClientes from "./tipoListagemClientes"
 import AtualizarClienteTitular from "./atualizarCliente"
+import DeletarClienteTitular from "./deletarClienteTitular"
 
 export default class Principal extends Processo {
     constructor() {
@@ -24,6 +25,10 @@ export default class Principal extends Processo {
                 break
             case 3:
                 this.processo = new TipoListagemClientes()
+                this.processo.processar()
+                break
+            case 4:
+                this.processo = new DeletarClienteTitular()
                 this.processo.processar()
                 break
             case 0:
